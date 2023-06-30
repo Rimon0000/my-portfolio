16a3cc2b497d2d482cebbba648caef8d75b546ac
Your Name <Rimon0000>
2023-06-30 12:45:13 +0600

    add banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
+import rimon from "../../../assets/rimon.jpeg"
+import {FaGithub, FaLinkedinIn, FaMailBulk } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+        <div className='flex justify-between gap-5 mt-5 pt-5'>
+            <div className='w-1/2'>
+                <h2 className='text-3xl font-semibold'>Hi everyone, I'm </h2>
+                <h1 className='text-4xl font-semibold'>Rimon Ron</h1>
+                <h3 className='text-3xl'>A passionate Software Engineer</h3>
+                <p className='text-3xl'>having an experience of building Modern Web Applications with some cool Libraries and Frameworks.</p>
+
+                <div className='flex gap-5 mt-5'>
+                <a href="https://github.com/Rimon0000"><FaGithub className='h-[43px] w-[43px] bg-slate-200 rounded-full p-1'></FaGithub></a>
+                <a href="https://www.linkedin.com/in/rimon-uddin-79b858238/"><FaLinkedinIn className='h-[43px] w-[43px] bg-slate-200 rounded-full p-1'></FaLinkedinIn></a>
+                <a href="mailTo:rimonron.ad@gmail.com"><FaMailBulk className='h-[43px] w-[43px] bg-slate-200 rounded-full p-1'></FaMailBulk></a>
+                </div>
+                <div className='mt-5'>
+                <a className="btn btn-outline btn-primary mr-3" href="https://drive.google.com/uc?id=1WEuRNXpFmOjjsQfMcUdUlXtl5jbkQWmv&export=download"download>Resume</a>
+                <a className="btn btn-outline btn-primary mr-3" href="">Contact Me</a>
+                </div>
+            </div>
+            <div className="avatar">
+                <div className="w-full h-[400px] rounded-full">
+                  <img className='mr-20' src={rimon} />
+                </div>
+            </div>
         </div>
     );
 };
