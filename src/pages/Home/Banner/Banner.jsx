681c5ce807de6a793d59252dd45bc0abb0592e57
Your Name <Rimon0000>
2023-07-01 10:57:26 +0600

    skills part done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import rimon from "../../../assets/rimon.jpeg"
 import {FaGithub, FaLinkedinIn, FaMailBulk } from 'react-icons/fa';
+import { Typewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
+
     return (
         <div className='flex justify-between gap-5 mt-5 pt-5'>
-            <div className='w-1/2'>
-                <h2 className='text-3xl font-semibold'>Hi everyone, I'm </h2>
-                <h1 className='text-4xl font-semibold'>Rimon Ron</h1>
-                <h3 className='text-3xl'>A passionate Software Engineer</h3>
-                <p className='text-3xl'>having an experience of building Modern Web Applications with some cool Libraries and Frameworks.</p>
+            <div>
+                <h2 className='text-4xl font-semibold text-blue-500	'>Hi everyone, I'm </h2>
+                <h1 className='text-4xl font-semibold mt-2'>Rimon Ron</h1>
+                <div className='flex gap-3 text-4xl mt-2'>
+                <h3 className='font-semibold'>A passionate</h3>
+                <span style={{ color: 'blue', fontWeight: 'bold' }}>
+                <Typewriter
+                  words={['Software Engineer', 'MERN Stack Developer', 'Frontend Developer']}
+                  loop={5}
+                  cursor
+                  cursorStyle='_'
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+                </span>
+                </div>
+                <p className='text-3xl font-semibold mt-2'>having an experience of building Modern Web Applications with some cool Libraries and Frameworks.</p>
 
-                <div className='flex gap-5 mt-5'>
+                <div className='flex gap-6 mt-5'>
                 <a href="https://github.com/Rimon0000"><FaGithub className='h-[43px] w-[43px] bg-slate-200 rounded-full p-1'></FaGithub></a>
                 <a href="https://www.linkedin.com/in/rimon-uddin-79b858238/"><FaLinkedinIn className='h-[43px] w-[43px] bg-slate-200 rounded-full p-1'></FaLinkedinIn></a>
                 <a href="mailTo:rimonron.ad@gmail.com"><FaMailBulk className='h-[43px] w-[43px] bg-slate-200 rounded-full p-1'></FaMailBulk></a>
---
+++ src/pages/Home/Skills/Skills.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ProgressBar from "@ramonak/react-progress-bar";
+
+const Skills = () => {
+    return (
+        <div>
+            <h2 className='text-center text-4xl font-bold m-5 p-5'>My Skills</h2>
+            <div className='grid lg:grid-cols-2 gap-5 mt-5'>
+                <div>
+                    <h2 className='text-2xl'>HTML</h2>
+                    <ProgressBar completed={90}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>CSS</h2>
+                    <ProgressBar completed={90}></ProgressBar>  
+                </div> 
+                <div>
+                    <h2 className='text-2xl'>Tailwind CSS</h2>
+                    <ProgressBar completed={80}></ProgressBar>  
+                </div> 
+                <div>
+                    <h2 className='text-2xl'>Bootstrap</h2>
+                    <ProgressBar completed={80}></ProgressBar>  
+                </div> 
+                <div>
+                    <h2 className='text-2xl'>Javascript</h2>
+                    <ProgressBar completed={75}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>React</h2>
+                    <ProgressBar completed={70}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>Node Js</h2>
+                    <ProgressBar completed={50}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>Express Js</h2>
+                    <ProgressBar completed={50}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>MongoDB</h2>
+                    <ProgressBar completed={55}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>PHP</h2>
+                    <ProgressBar completed={40}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>Laravel</h2>
+                    <ProgressBar completed={50}></ProgressBar>  
+                </div>
+                <div>
+                    <h2 className='text-2xl'>MySQL</h2>
+                    <ProgressBar completed={40}></ProgressBar>  
+                </div> 
+            </div>
+        </div>
+    );
+};
+
+export default Skills;
