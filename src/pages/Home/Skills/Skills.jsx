e820664e693d8392721749c29f480c172cd636a0
Your Name <rimonron.ad@gmail.com>
2024-10-03 14:09:02 +0600

    skills updated

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -15,14 +15,22 @@ const Skills = () => {
       <h2 className="text-center text-4xl font-bold m-5 p-5 border-b-4 border-gray-500 w-1/2 mx-auto">
         My Skills
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 mt-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 mt-5">
         {data?.data?.map((item) => (
-          <div>
-            <h2 className="text-2xl">{item.title}</h2>
-            <ProgressBar
-              className="progress"
-              completed={item.progress}
-            ></ProgressBar>
+          <div className="card bg-base-200 shadow-xl px-5 pt-1 pb-2 border hover:border-blue-500 transition-all duration-400 hover:rounded-md ">
+            <figure className="text-center">
+              <img
+                src={item.icon}
+                alt="Icon"
+                className="rounded-xl max-w-[8%]" />
+              <h2 className="card-title ml-3">{item.title}</h2>
+            </figure>
+            <div className="mt-2">
+              <ProgressBar
+               className="progress"
+                completed={item.progress}
+              ></ProgressBar>
+            </div>
           </div>
         ))}
       </div>
